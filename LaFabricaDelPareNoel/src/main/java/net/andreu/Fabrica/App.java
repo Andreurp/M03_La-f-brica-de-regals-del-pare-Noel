9defb6da54d81c9aada00d3de5da314e5affa2ce
Andreu <Andreu@Andreu1>
2015-12-22 16:56:14 +0100

    commit inicial

---
+++ LaFabricaDelPareNoel/src/main/java/net/andreu/Fabrica/App.java
@@ -0,0 +1,13 @@
+package net.andreu.Fabrica;
+
+/**
+ * La fàbrica del pare Noel...
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
